610d7dc411f94d270683e9b778534d98370edb47
yuanyuan7500-gif <yuanyuan7500@gmail.com>
2026-05-08 11:08:56 +0800

    Update vite.config.ts
    改善vite配置版本兼容性

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
+
 // https://vite.dev/config/
 export default defineConfig({
   base: './',
@@ -11,4 +12,10 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    target: ['es2020', 'edge88', 'firefox78', 'chrome87', 'safari14'],
+    // 或者更保守：
+    // target: 'es2015',
+    cssTarget: ['chrome61', 'firefox60', 'safari11', 'edge79'],
+  }
 });
